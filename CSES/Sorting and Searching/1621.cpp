0d6303dd4eb3ff7a4bd67b6b7ef3884c1eb89456
Rainmaker2627 <141350610+Rainmaker2627@users.noreply.github.com>
2023-11-14 21:03:47 +0200

    first code commit

---
+++ CSES/Sorting and Searching/1621.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+set<int> x;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> n;
+	int b, c=0;
+	for (int i = 0; i < n; ++i) {
+		cin >> b;
+		if (x.count(b)) continue;
+		c++;
+		x.insert(b);
+	}
+
+	cout << c;
+	return 0;
+}
